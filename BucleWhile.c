b89a157efb1c576cd1e145fda204bbe4178cabf4
Flarrica <107935237+Flarrica@users.noreply.github.com>
2025-03-26 12:58:19 -0300

    Ej6Palindromo

---
+++ BucleWhile.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char const *argv[])
+{
+    int numero;
+    int prueba;
+    int inverso;
+
+    printf("Ingrese un numero: \n");
+    scanf("%d", &numero);
+    prueba = numero;
+    
+
+    while (prueba > 0){
+        inverso = (inverso * 10) + prueba % 10;
+        prueba = prueba / 10;
+    }
+
+    if (inverso == numero){
+        printf("El numero es un palindromo! \n");
+    } else{
+        printf("El numero no es un palindromo. \n");
+    }
+    
+    return 0;
+}
