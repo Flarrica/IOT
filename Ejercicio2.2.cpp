bd42c4415bef0b8e9240e117c469e77608357138
MG2811 <marcos.giombini@correo.ucu.edu.uy>
2025-03-27 13:37:44 -0300

    Ejercicio2
    Ejercicio de array de los propuestos en clase
    
    Co-Authored-By: Flarrica <107935237+Flarrica@users.noreply.github.com>
    Co-Authored-By: diegomassaferro <169001067+diegomassaferro@users.noreply.github.com>

---
+++ Ejercicio2.2.cpp
@@ -0,0 +1,56 @@
+
+/*
+    2. Escribe un programa en C para encontrar los elementos máximo y mínimo en un array.
+*/
+
+#include<iostream>
+    using namespace std;
+
+    int main() {
+        int numeros[10]; // reserva la cantidad de lugares.
+        int n;
+        do
+        {
+            cout<< "\nIngrese la cantidad de numeros a colocar (1-10): ";
+            cin>> n;
+        } while (n < 1|| n > 10);
+        for (int i = 0; i < n; i++)
+        {
+            cout<< "\nIngrese el valor para la posicion "<< (i+1)<< ": ";
+            cin >> numeros[i];
+        }
+        cout<< "\n Los valores de los vectores son: "<<endl;
+        
+        for (int i = 0; i < n; i++)
+        {
+            cout<< i+1<< " -> " << numeros[i]<<endl;   
+         }
+        cout<< "\n-------------------------------------------------------";
+
+        int mayor = numeros[0];
+        int pos= 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (numeros[i] > mayor )
+            {
+                mayor = numeros [i];
+                pos = i;
+            }
+            
+        }
+        cout << "\nEl mayor valor del arreglo es: " << mayor << " La posicion es: "<<pos+1<< endl;
+        cout<< "\n-------------------------------------------------------";
+
+        int menor=numeros[0];
+        for (int i = 0; i < n; i++)
+        {
+            if (numeros[i]<menor)
+            {
+               menor = numeros[i];
+               pos= i;
+            }
+            
+        }
+        cout<< "\nEL menor valor del arreglo es: " << menor << " La posicion es: " << pos + 1<< endl;
+        return 0;
+    }
