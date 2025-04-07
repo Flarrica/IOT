c5792941f27cac6e90b52f8e0f6e062c28b530e9
Flarrica <107935237+Flarrica@users.noreply.github.com>
2025-04-06 23:46:37 -0300

    primeraParteMinyMax
    Posiblemente no funque pero depende del test que le hagan

--- Laboratorio1/components/my_lib_1_2/include/my_lib_1_2.h
+++ Laboratorio1/components/my_lib_1_2/include/my_lib_1_2.h
@@ -3,23 +3,28 @@
 
 #include <stdio.h>
 #include <math.h>
-#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
 
 void my_lib_1_2_function();
 root_t eq_solver(coeff_t *coeficientes);
+int32_t bin2dec(int32_t binary, bool sign);
+void print_reverse_array(void *array, size_t data_type, size_t array_size);
+void max_index(void *array, size_t data_type, size_t array_size);
+void min_index(void *array, size_t data_type, size_t array_size);
 
-typedef struct {
-    double a;
-    double b;
-    double c;
-} coeff_t;
+
 
 typedef struct {
     double R1;
     double R2;
 } root_t;
 
-
+typedef struct {
+    double a;
+    double b;
+    double c;
+} coeff_t;
 
 
 #endif
--- Laboratorio1/components/my_lib_1_2/my_lib_1_2.c
+++ Laboratorio1/components/my_lib_1_2/my_lib_1_2.c
@@ -5,7 +5,7 @@ void  init_lab(void);
 
 void  init_lab() {
     char nombre1[] ="Fernando Fabian Larrica Gastan";
-    char nombre2[] = "Gaston Diego Massaferro";
+    char nombre2[] = "Diego Gaston Massaferro Molina";
     char nombre3[] = "Marcos Giombini";
 
     printf("Laboratorio lenguaje C de %s, %s y %s.\n", nombre3,nombre2,nombre1);
@@ -50,4 +50,121 @@ root_t eq_solver(coeff_t *coeficientes){
         printf("x2 = %.2f - %.2fi\n", resultado.R1, resultado.R2);
     }
     return resultado;
+}
+
+int32_t bin2dec(int32_t binary, bool sign){
+    int32_t decimal = 0;
+    int contador = 0;
+    int exponente = 0;
+    int32_t original = binary;
+
+    while (binary > 0) {
+        int digito = binary % 10;
+        if (digito != 0 && digito != 1) {
+            printf("No ha ingresado un numero binario valido. \n");
+            return 0;
+        }
+        if ((binary & 1) == 1){
+
+            exponente = pow(2,contador);
+            contador++;
+            decimal += exponente;
+            binary = binary >> 1;
+        }
+        else{
+            binary = binary >> 1;
+            contador++;
+        }
+    }
+    if (sign){
+        if ((original >> (contador - 1)) & 1) {
+            exponente = pow(2,contador);
+            decimal -= exponente;
+            printf("El numero binario pasado a decimal es : - %d \n", decimal);
+        }
+    }
+    printf("El numero binario pasado a decimal es : %d \n", decimal);
+    return ;
+}
+
+void print_reverse_array(void *array, size_t data_type, size_t array_size){
+    uint8_t *inversoAlgo = (uint8_t *)array;
+
+    printf("Array en orden inverso:\n");
+
+    for (int i = array_size - 1; i >= 0; i--) {
+        void *element = inversoAlgo + i * data_type;
+
+        if (data_type == sizeof(int)) {
+            printf("%d ", *(int *)element);
+        } else if (data_type == sizeof(float)) {
+            printf("%.2f ", *(float *)element);
+        } else if (data_type == sizeof(double)) {
+            printf("%.2lf ", *(double *)element);
+        } else if (data_type == sizeof(char)) {
+            printf("%c ", *(char *)element);
+        } else {
+            printf("Tipo desconocido ");
+        }
+    }
+
+    printf("\n");
+    return;
+}
+
+void max_index(void *array, size_t data_type, size_t array_size){
+    uint8_t *ptr = (uint8_t *)array;
+    size_t max_idx = 0;
+
+    if (array_size == 0) return;
+
+    for (size_t i = 1; i < array_size; i++) {
+        void *current = ptr + i * data_type;
+        void *max = ptr + max_idx * data_type;
+
+        if (data_type == sizeof(int)) {
+            if (*(int *)current > *(int *)max)
+                max_idx = i;
+        } else if (data_type == sizeof(float)) {
+            if (*(float *)current > *(float *)max)
+                max_idx = i;
+        } else if (data_type == sizeof(double)) {
+            if (*(double *)current > *(double *)max)
+                max_idx = i;
+        } else if (data_type == sizeof(char)) {
+            if (*(char *)current > *(char *)max)
+                max_idx = i;
+        }
+    }
+
+    printf("Máximo en índice: %zu\n", max_idx);
+return;
+}
+void min_index(void *array, size_t data_type, size_t array_size){
+    uint8_t *ptr = (uint8_t *)array;
+    size_t min_idx = 0;
+
+    if (array_size == 0) return;
+
+    for (size_t i = 1; i < array_size; i++) {
+        void *current = ptr + i * data_type;
+        void * min = ptr +  min_idx * data_type;
+
+        if (data_type == sizeof(int)) {
+            if (*(int *)current < *(int *) min)
+                min_idx = i;
+        } else if (data_type == sizeof(float)) {
+            if (*(float *)current < *(float *) min)
+                min_idx = i;
+        } else if (data_type == sizeof(double)) {
+            if (*(double *)current < *(double *) min)
+                min_idx = i;
+        } else if (data_type == sizeof(char)) {
+            if (*(char *)current < *(char *) min)
+                min_idx = i;
+        }
+    }
+
+    printf("Mínimo en índice: %zu\n",  min_idx);
+return;
 }
