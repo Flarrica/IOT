79e844013779cb227c693a2b678ba9175989f726
Flarrica <107935237+Flarrica@users.noreply.github.com>
2025-07-02 17:34:34 -0300

    LogerAudioEvent
    en la librería audio cuando manda a la cola manda al logger, puede q tenga q cambiarlo para una mejor respuesta

--- Lab_Final_1/components/audio_player/src/audio_player.c
+++ Lab_Final_1/components/audio_player/src/audio_player.c
@@ -23,6 +23,7 @@
 #include "driver/i2s_std.h"
 #include "driver/gpio.h"
 #include "es8311.h"
+#include "logger.h"
 
 #include "esp_spiffs.h"
 
@@ -192,6 +193,25 @@ audio_state_t audio_player_get_state(void) {
 void audio_player_send_cmd(audio_cmd_t cmd) {
     if (audio_event_queue != NULL) {
         xQueueSend(audio_event_queue, &cmd, 0);
+        switch (cmd) {
+        case CMD_PLAY:
+            logger_add_event(LOGGER_EVENT_PLAY);
+            break;
+        case CMD_PAUSE:
+            logger_add_event(LOGGER_EVENT_PAUSE);
+            break;
+        case CMD_STOP:
+            logger_add_event(LOGGER_EVENT_STOP);
+            break;
+        case CMD_NEXT:
+            logger_add_event(LOGGER_EVENT_NEXT);
+            break;
+        case CMD_PREV:
+            logger_add_event(LOGGER_EVENT_PREVIOUS);
+            break;
+        default:
+            break;
+    }
 
     } else {
         ESP_LOGW(TAG, "Cola de comandos no inicializada");
